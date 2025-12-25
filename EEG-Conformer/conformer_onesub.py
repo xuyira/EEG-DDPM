2b2a3fe07fd82b7da320d50adf6e6d459d3d355d
xuyira <1564492262@qq.com>
2025-12-26 00:25:45 +0800

    单受试

--- EEG-Conformer/conformer_onesub.py
+++ EEG-Conformer/conformer_onesub.py
@@ -224,7 +224,7 @@ class ExP():
         self.nSub = nsub
 
         self.start_epoch = 0
-        self.root = '/Data/strict_TE/'
+        self.root = './data/standard_2a_data/'
 
         self.log_write = open("./results/log_subject%d.txt" % self.nSub, "w")
 
