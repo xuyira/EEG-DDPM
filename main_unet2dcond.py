80d3496a95239423413eb63a543d99ae922153da
xuyira <1564492262@qq.com>
2025-12-29 21:08:12 +0800

    不知道改哪了

--- main_unet2dcond.py
+++ main_unet2dcond.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 import gc
 from diffusers.optimization import get_cosine_schedule_with_warmup
 import numpy as np
+import pandas as pd
 
 from config import TrainingConfig
 from model.unet2dcond import create_unet2dcond_model
